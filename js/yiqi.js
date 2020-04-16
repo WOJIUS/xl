1f7de542cc770418bdd6e6fb0584319f641479db
WOJIUS <63736155+WOJIUS@users.noreply.github.com>
2020-04-16 15:42:30 +0800

    Add files via upload

---
+++ js/yiqi.js
@@ -0,0 +1,34 @@
+﻿// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		Dianji++;
+		if(Dianji==1){
+	   			alert("我真的喜欢你");
+	   }else if(Dianji==2){
+		   		alert("别不好意思啦");
+		   		
+	   }else if(Dianji==3){
+		   		alert("宠你一辈子");
+		   		
+	   }else if(Dianji==4){
+		   		alert("家务我全干");
+		   		
+	  }else if(Dianji==5){
+		   		alert("不藏私房钱");
+	  }else if(Dianji==6){
+		   		alert("房子写你名");
+	  }else if(Dianji==7){
+		   		alert("工资全上交");
+		  		Dianji=1;
+	  }
+
+	}
+	hao.onclick=function(){
+		alert("嘿嘿，爱你");
+	}
+	
+
+}
